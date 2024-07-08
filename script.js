818de66b662fe762b2948261b0e9cc2c2f2c5e43
play3123 <129664998+play3123@users.noreply.github.com>
2024-07-08 16:28:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    generateLadder();
-});
-
 function generateLadder() {
-    const numChoices = 46;
+    const numParticipants = document.getElementById('numParticipants').value;
     const ladderContainer = document.getElementById('ladder-container');
     ladderContainer.innerHTML = '';
-
-    const choices = Array(42).fill('일반').concat(Array(4).fill('리저브'));
+    const choices = Array(parseInt(numParticipants) - 4).fill('일반').concat(Array(4).fill('리저브'));
     shuffle(choices);
 
-    for (let i = 0; i < numChoices; i++) {
+    for (let i = 0; i < numParticipants; i++) {
         const column = document.createElement('div');
         column.className = 'column';
-        column.dataset.result = choices[i];
+        column.dataset.result = choices[i % choices.length];
 
         for (let j = 0; j < 10; j++) {
             const step = document.createElement('div');
             step.className = 'step';
 
-            if (Math.random() > 0.7) {
+            if (Math.random() > 0.7 && i < numParticipants - 1) {
                 const horizontal = document.createElement('div');
                 horizontal.className = 'horizontal';
                 step.appendChild(horizontal);
